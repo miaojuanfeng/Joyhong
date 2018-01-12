961d318bd88b8044ed035c52ed2480089e4ad044
miaojuanfeng <miaojuanfeng@qq.com>
2018-01-12 20:11:45 +0800

    asd

--- src/main/java/com/joyhong/api/UploadController.java
+++ src/main/java/com/joyhong/api/UploadController.java
@@ -1,12 +1,9 @@
 package com.joyhong.api;
 
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
-import java.net.HttpURLConnection;
 import java.net.URLDecoder;
 import java.util.Date;
 
@@ -20,43 +17,60 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.joyhong.model.Upload;
 import com.joyhong.service.UploadService;
+import com.joyhong.service.common.MD5Service;
 
 import net.sf.json.JSONObject;
 
 /**
  * 文件上传控制器
  * @author user
- *
  */
 @Controller
+@RequestMapping(value="/upload")
 public class UploadController {
 	
 	@Autowired
 	private UploadService uploadService;
 	
-	private String tempPath = "/home/wwwroot/default/upload/";
-	private String filePath = "/home/wwwroot/default/upload/";
-//	private String tempPath = "/Users/user/Desktop/";
-//	private String filePath = "/Users/user/Desktop/";
+	@Autowired
+	private MD5Service md5Service;
+	
+//	private String tempPath = "/home/wwwroot/default/upload/";
+//	private String filePath = "/home/wwwroot/default/upload/";
+	private String tempPath = "/Users/user/Desktop/";
+	private String filePath = "/Users/user/Desktop/";
 	private String fileUrl = "http://47.89.32.89/upload/";
 	
 	/**
-	 * 上传文件
+	 * 上传视频
 	 * @param request
 	 * @return
 	 * @throws Exception
 	 */
-	@RequestMapping(value="/upload", method = RequestMethod.POST)
+	@RequestMapping(value="/video", method = RequestMethod.POST)
 	@ResponseBody
-	public String upload(HttpServletRequest request) throws Exception {
+	public String video(HttpServletRequest request) throws Exception {
 		JSONObject retval = new JSONObject();
+		JSONObject temp = new JSONObject();
 		
+		/*
+         * 获取MD5
+         */
+		String fileMD5 = request.getHeader("File-MD5");
+		if( null != fileMD5 ){
+			fileMD5 = URLDecoder.decode(fileMD5, "UTF-8");
+		}else{
+			retval.put("status", false);
+			retval.put("msg", "File-MD5 property is not set or format error");
+			return retval.toString();
+		}
 		/*
 		 * 获取文件大小
 		 */
-		String fileSize = URLDecoder.decode(request.getHeader("File-Size"), "UTF-8");
+		String fileSize = request.getHeader("File-Size");
 		int endPoint = 0;
 		if( null != fileSize ){
+			fileSize = URLDecoder.decode(fileSize, "UTF-8");
 			try{
 				endPoint = Integer.parseInt(fileSize);
 			}catch(Exception e){
@@ -72,10 +86,11 @@ public class UploadController {
 		/*
 		 * 获取字节范围
 		 */
-		String range = URLDecoder.decode(request.getHeader("File-Range"), "UTF-8");
+		String range = request.getHeader("File-Range");
         int start = 0;
         int end = 0;
         if( null != range && range.startsWith("bytes=") ){
+        	range = URLDecoder.decode(range, "UTF-8");
             String[] values = range.split("=")[1].split("-");
         	try{
         		start = Integer.parseInt(values[0]);
@@ -118,8 +133,9 @@ public class UploadController {
         /*
          * 获取文件名
          */
-        String fileName = URLDecoder.decode(request.getHeader("Content-Disposition"), "UTF-8");
+        String fileName = request.getHeader("Content-Disposition");
         if( null != fileName ){
+        	fileName = URLDecoder.decode(fileName, "UTF-8");
 	        fileName = new String(fileName.getBytes("ISO8859-1"), "UTF-8");
 	        int index = fileName.lastIndexOf("filename=");
 	        if( index > -1 ){
@@ -144,11 +160,28 @@ public class UploadController {
         }
         /*
          * 检查文件是否存在
+         * 根据文件MD5值检查，相同MD5值则不写文件直接推送
+         */
+        File existsFile = new File(filePath + fileName);
+        if( existsFile.exists() ){
+        	if( fileMD5.equals(md5Service.getMD5OfFile(filePath + fileName)) ){
+        		/*
+        		 * 推送在此
+        		 */
+        		retval.put("status", true);
+				temp.put("complete", true);
+				temp.put("file", fileUrl + fileName);
+				retval.put("data", temp);
+				return retval.toString();
+        	}
+        }
+        /*
+         * 检查临时文件是否存在
          * 如果start为1表示需要重写文件，将已上传的数据块删除
          */
-        File existsFile = new File(tempPath + fileName + ".temp");
-        if( start == 1 && existsFile.exists() ){
-        	existsFile.delete();
+        File existsTempFile = new File(tempPath + fileName + ".temp");
+        if( start == 1 && existsTempFile.exists() ){
+        	existsTempFile.delete();
         }
         /*
          * 追加文件
@@ -166,15 +199,15 @@ public class UploadController {
 	    in.close();
 		
 		oSavedFile.close();
-        
-		JSONObject temp = new JSONObject();
+       
 		if( end == endPoint ){
 			/*
     		 * 获取用户id
     		 */
-    		String userId = URLDecoder.decode(request.getHeader("User-Id"), "UTF-8");
+    		String userId = request.getHeader("User-Id");
     		int user_id = 0;
     		if( null != userId ){
+    			userId = URLDecoder.decode(userId, "UTF-8");
     			try{
     				user_id = Integer.parseInt(userId);
     			}catch(Exception e){
@@ -188,11 +221,35 @@ public class UploadController {
     			return retval.toString();
     		}
     		/*
+             * 获取device id
+             */
+    		String deviceStr = request.getHeader("Device-Id");
+    		Integer[] device_id = null;
+    		if( null != deviceStr ){
+    			deviceStr = URLDecoder.decode(deviceStr, "UTF-8");
+    			String[] deviceArr = deviceStr.split(",");
+    			device_id = new Integer[deviceArr.length];
+    			for(int i=0;i<deviceArr.length;i++){
+	    			try{
+	    				device_id[i] = Integer.valueOf(deviceArr[i]);
+	    			}catch(Exception e){
+	    				retval.put("status", false);
+	    				retval.put("msg", "Device-Id property format error");
+	    				return retval.toString();
+	    			}
+    			}
+    		}else{
+    			retval.put("status", false);
+    			retval.put("msg", "Device-Id property is not set");
+    			return retval.toString();
+    		}
+    		/*
     		 * 获取文件描述
     		 */
-    		String fileDesc = URLDecoder.decode(request.getHeader("File-Desc"), "UTF-8");
+    		String fileDesc = request.getHeader("File-Desc");
     		String file_desc = "";
     		if( null != fileDesc ){
+    			fileDesc = URLDecoder.decode(fileDesc, "UTF-8");
     			file_desc = fileDesc;
     		}
     		/*
@@ -208,6 +265,9 @@ public class UploadController {
 				upload.setModifyDate(new Date());
 				upload.setDeleted(0);
 				if( uploadService.insert(upload) == 1 ){
+					/*
+	        		 * 推送在此
+	        		 */
 					retval.put("status", true);
 					temp.put("complete", true);
 					temp.put("file", fileUrl + fileName);
@@ -229,8 +289,18 @@ public class UploadController {
 			retval.put("data", temp);
 		}
 		
-        
         return retval.toString();
+	}
+	
+	@RequestMapping(value="/image", method = RequestMethod.POST)
+	@ResponseBody
+	public String image(HttpServletRequest request) throws Exception {
+		JSONObject retval = new JSONObject();
+		
+		String aa = md5Service.getMD5OfFile(filePath + "20160725134606_LG5FH.jpg");  
+        System.out.println(aa);
+		
+		return retval.toString();
 	}
 	
 	/**
